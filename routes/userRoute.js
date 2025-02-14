43c03d4b0caa09bbacaa186dc166c4327eec2e3e
dennis dentrix <denkyusya@gmail.com>
2025-02-14 08:08:05 +0300

    auth status

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,7 +5,7 @@ const authController = require("../controllers/authController");
 
 // Add this route to check authentication status
 
-router.get('/status', authController.protect, authController.checkAuthStatus);
+router.get('/status', authController.checkAuthStatus);
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
