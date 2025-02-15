7066f49b6f92476dcf1c976dc61d08480f5104cd
dennis dentrix <denkyusya@gmail.com>
2025-02-15 08:30:15 +0300

    route unprotected

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const listController = require("../controllers/listController")
 const authController = require("../controllers/authController");
 
-router.route('/').post(listController.createItem).get(authController.protect, listController.getAllItems)
+router.route('/').post(listController.createItem).get(listController.getAllItems)
 router.route('/:id').get(listController.getAnItem).patch(listController.updateItem)
 router.route('/:id').delete(listController.deleteItem)
 
