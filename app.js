93969faa64c1c33a9794e3def63a1af4af2b7dd1
dennis dentrix <denkyusya@gmail.com>
2025-02-20 21:13:37 +0300

    refactor: update static file serving from 'dist' to 'build' directory

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ app.use(xss());
 
 // app.use(compression());
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use("/api/v1/list", listRouter);
